f79e22fa0c12bb441c0f93a774f501f2905480b8
Prashant Rao <prashant.rao5321@gmail.com>
2025-11-20 21:26:43 -0600

    Updated RAG function to take metadata filters

--- support/nodes.py
+++ support/nodes.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from .logger import Logging
+from logger import Logging
 from openai import OpenAI
 from typing import List
 from time import time
@@ -12,9 +12,15 @@ load_dotenv()
 
 client = OpenAI()
 Logging.setLevel()
+
 chroma_client = chromadb.PersistentClient(path="vectorDB")
 collection = chroma_client.get_or_create_collection(name="electronics")
-
+# chroma_client = chromadb.CloudClient(
+#   api_key=os.environ.get("CHROMA_API_KEY"),
+#   tenant=os.environ.get("TENANT_KEY"),
+#   database='ProdLens_ChromaDB'
+# )
+# collection = chroma_client.get_or_create_collection(name="ProdLens_ChromaDB")
 
 def text_to_sql(prompt: str) -> str:
     try:
@@ -22,6 +28,7 @@ def text_to_sql(prompt: str) -> str:
         schema_context = open("schema.sql", "r").read()
 
         system_prompt = open(os.path.join("templates", "text2sql.txt")).read()
+
 
         resp = client.chat.completions.create(
             model="gpt-4o",
@@ -34,7 +41,8 @@ def text_to_sql(prompt: str) -> str:
         )
         assistant_text = resp.choices[0].message.content.strip()
         sql = re.sub(r"```(?:sql)?\s*([\s\S]*?)```", r"\1", assistant_text)
-        Logging.logDebug(f"SQL Query:\n{sql}\n")
+        # if "product_id" not in sql
+        Logging.logInfo(f"SQL Query:\n{sql}\n")
         return sql
     except Exception as e:
         Logging.logError(str(e))
@@ -54,7 +62,7 @@ def get_embeddings(texts: List[str], model="text-embedding-3-small") -> List[np.
         raise e 
 
 
-def rag_query(prompt: str, content_type: str = "spec") -> dict:
+def rag_query(prompt: str, content_type: str = "spec", product_id: int = None) -> dict:
     try:
         Logging.logDebug(f"Retrieving RAG context for the prompt: {prompt}")
         query_embedding = client.embeddings.create(
@@ -62,11 +70,17 @@ def rag_query(prompt: str, content_type: str = "spec") -> dict:
             model="text-embedding-3-small"
         ).data[0].embedding
 
+        if content_type == "spec":
+            condition = {"type": "spec"}
+        else:
+            assert product_id is not None, "product_id is not provided to fetch reviews"
+            condition = {"$and": [{"type": "reviews"}, {"product_id": product_id}]}
+            
         start = time()
         results = collection.query(
             query_embeddings=[query_embedding],
             n_results=3,
-            where={"type": content_type}
+            where=condition
         )
         Logging.logInfo(f"Time taken to fetch response: {round(time() - start, 2)} seconds.")
         Logging.logDebug(f"RAG Results:\n{results}\n")
@@ -78,4 +92,4 @@ def rag_query(prompt: str, content_type: str = "spec") -> dict:
 
 if __name__ == "__main__":
     print(text_to_sql("Suggest me some monitors with high refresh rates for gaming."))
-    print(rag_query("What is RGB in Monitors", "reviews"))
+    print(rag_query("What is RGB in Monitors", "spec"))
